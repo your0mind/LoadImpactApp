f4a153931570ddf139f75d830aa0ce763af6a981
Kiselev Denis <denis.kiselev.1997@gmail.com>
2018-09-24 15:57:09 +0300

    little fix for last commit

--- LoadImpactApp/MainForm.cs
+++ LoadImpactApp/MainForm.cs
@@ -286,10 +286,10 @@ namespace LoadImpactApp
             testResultsDataGridView.Rows[index].Cells[1].Value = (metricSettings.Min)
                 ? (object)Math.Round(mc.Min(), metricSettings.Precision)
                 : (object)"-";
-            testResultsDataGridView.Rows[index].Cells[1].Value = (metricSettings.Median)
+            testResultsDataGridView.Rows[index].Cells[2].Value = (metricSettings.Median)
                 ? (object)Math.Round(mc.Median(), metricSettings.Precision)
                 : (object)"-";
-            testResultsDataGridView.Rows[index].Cells[1].Value = (metricSettings.Max)
+            testResultsDataGridView.Rows[index].Cells[3].Value = (metricSettings.Max)
                 ? (object)Math.Round(mc.Max(), metricSettings.Precision)
                 : (object)"-";
             testResultsDataGridView.Rows[index].Cells[4].Value = unit;
